e30ebf32a193da17edd45db837d00facd08e22df
Erik Dalén <dalen@spotify.com>
2012-11-21 15:16:37 +0100

    Add reverse lookups with a caching mechanism

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 hieralookup
 ===========
 
-A web service for doing hiera lookups
+A web service for doing hiera lookups and reverse hiera lookups
 
-It gets facts from Puppet's inventory service and looks up the specified hiera key.
+It gets facts from PuppetDB and looks up the specified hiera key.
 Results are returned as JSON.
 
-URL format is:
+Forward lookup
+--------------
 
 /hiera/&lt;hostname&gt;/&lt;key&gt;[/&lt;resolution_type&gt;][?fact=override]
 
@@ -14,3 +15,12 @@ URL format is:
 - __key__ is the hiera key to look up
 - __resolution_type__ is optional and should be either _array_, _priority_ or _hash_ with _priority_ being the default
 - Any additional URL parameters are used for overriding facts
+
+Reverse lookup
+--------------
+
+/hiera_reverse/&lt;key&gt;/&lt;value&gt;[/&lt;resolution_type&gt;]
+
+- __key__ is the hiera key to look up
+- __value__ is the value to match
+- __resolution_type__ is optional and should be either _array_, _priority_ or _hash_ with _priority_ being the default
--- hieralookup.rb
+++ hieralookup.rb
@@ -4,12 +4,15 @@ require 'hiera'
 require 'puppet'
 require 'json'
 require 'sinatra'
+require 'time'
 
 hiera = Hiera.new
 Hiera.logger='noop'
 Puppet.initialize_settings
 # this can be :rest to use inventory service instead
 Puppet::Node::Facts.indirection.terminus_class = :puppetdb
+cache = {}
+cache_time = 1800
 
 get '/hiera/:_host/:_key/?:_resolution_type?' do
   scope = Puppet::Node::Facts.indirection.find(params[:_host])
@@ -21,4 +24,24 @@ get '/hiera/:_host/:_key/?:_resolution_type?' do
   next [404, 'Key not found'] if res == nil
   out = JSON.generate(res)
   $? == 0 ? out : [500, out]
+end
+
+# This does a reverse lookup to see which node has a certain value
+# it is quite slow but caches each value for cache_time seconds
+get '/hiera_reverse/:key/:value/?:_resolution_type?' do |key,value,resolution_type|
+  res = []
+  Puppet::Node::Facts.indirection.search('').each do |node|
+    cache[key] = {} unless cache.include? key
+    unless cache[key].include? node and cache[key][node][:time] > Time.now.to_i - cache_time
+      scope = Puppet::Node::Facts.indirection.find(node)
+    scope = scope.values if scope.is_a?(Puppet::Node::Facts)
+      next if scope == nil
+      cache[key][node] = {
+        :value => hiera.lookup(key, [], scope, nil, resolution_type ? resolution_type.to_sym : :priority),
+        :time  => Time.now.to_i,
+      }
+    end
+    res << node if cache[key][node][:value].include? value
+  end
+  JSON.generate(res)
 end
